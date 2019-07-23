1254b8eab551d1cbcb33600f223fb46655884893
Ademir Besirovic <ademir.besirovic1996@gmail.com>
2019-07-23 13:15:11 +0200

    setup ApolloClient

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
+import config from './config';
+
+const { uri } = config.apollo;
+const client = new ApolloClient({
+  uri,
+});
+
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
-  );
+  return <ApolloProvider client={client} />;
 }
 
 export default App;
