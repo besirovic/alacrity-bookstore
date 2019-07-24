5310ae4e42bafaa509d115c7de767e2eba21b1a4
Ademir Besirovic <ademir.besirovic1996@gmail.com>
2019-07-24 17:13:14 +0200

    fix wrong default apollo server uri

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 export default {
   apollo: {
-    uri: process.env.REACT_APP_APOLLO_SERVER_URI || 'http://localhost:4567',
+    uri: process.env.REACT_APP_APOLLO_SERVER_URI || 'http://localhost:4567/graphql',
   },
 };
