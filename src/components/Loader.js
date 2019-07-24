a71bbdaa454e49107ea8b938702f6fd40f999265
Ademir Besirovic <ademir.besirovic1996@gmail.com>
2019-07-24 17:12:42 +0200

    implement Loader component

---
+++ src/components/Loader.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Heading } from 'grommet';
+
+const Loader = () => (
+  <Heading level={1} primary textAlign="center" margin={{ horizontal: 'auto', vertical: 'xlarge' }}>
+    Loading
+  </Heading>
+);
+
+export default Loader;
