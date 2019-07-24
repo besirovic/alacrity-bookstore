9c19e8a457a635f5b089680057c25746e0ce24f4
Ademir Besirovic <ademir.besirovic1996@gmail.com>
2019-07-24 17:14:15 +0200

    implement create book page

--- src/pages/books/Create.js
+++ src/pages/books/Create.js
@@ -1,15 +1,49 @@
 import React from 'react';
-import { Heading } from 'grommet';
+import PropTypes from 'prop-types';
+import { Box, Heading } from 'grommet';
+import { Mutation } from 'react-apollo';
 
 import Header from '../../components/Header';
+import BookForm from '../../components/BookForm';
 
-const CreateBook = () => (
-  <React.Fragment>
-    <Header />
-    <Heading level={1} color="brand" textAlign="center" margin="auto">
-      Create book page
-    </Heading>
-  </React.Fragment>
-);
+import { GET_BOOKS } from '../../graphql/queries/books';
+import { CREATE_BOOK } from '../../graphql/mutations/books';
+
+const updateCache = (cache, { data: { createBook } }) => {
+  const { books } = cache.readQuery({ query: GET_BOOKS });
+  cache.writeQuery({
+    query: GET_BOOKS,
+    data: { books: books.concat([createBook]) },
+  });
+};
+
+const CreateBook = ({ history }) => {
+  return (
+    <Mutation mutation={CREATE_BOOK} update={updateCache}>
+      {(createBook, { loading, error }) => {
+        const onSubmit = async variables => {
+          await createBook({ variables });
+          if (!loading && !error) history.push('/');
+        };
+
+        return (
+          <React.Fragment>
+            <Header />
+            <Box width="xlarge" margin="auto">
+              <Heading level={3} color="brand" textAlign="start">
+                Create new book
+              </Heading>
+              <BookForm error={error} loading={loading} onSubmit={onSubmit} />
+            </Box>
+          </React.Fragment>
+        );
+      }}
+    </Mutation>
+  );
+};
+
+CreateBook.propTypes = {
+  history: PropTypes.object,
+};
 
 export default CreateBook;
